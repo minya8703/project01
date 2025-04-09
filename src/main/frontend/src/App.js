9853910ad1447a92d68a725607a7ea6b64a0401d
minya8703 <minya8703@gmail.com>
2025-04-09 21:09:22 +0900

    Initial commit: Spring Boot + React project setup

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Spring Boot + React App</h1>
+        <p>
+          This is a sample application using Spring Boot and React.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
